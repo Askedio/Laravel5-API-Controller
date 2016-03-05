d29c76789efb088f9883a6e657a7da347f81cddc
William <gcphost@users.noreply.github.com>
2016-03-04 21:46:26 -0800

    Update routes.php

--- tests/App/Http/routes.php
+++ tests/App/Http/routes.php
@@ -10,9 +10,6 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::group(['prefix' => 'api', 'middleware' => ['web', 'api']], function () {
   Route::resource('/user', 'Askedio\Tests\App\Http\Controllers\TestController');
