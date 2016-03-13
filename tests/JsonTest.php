5f2bf926e91c85317d25d9c43ce6b6b75ccbbee4
William <gcphost@gmail.com>
2016-03-12 21:45:46 -0500

    Applied fixes from StyleCI

--- tests/JsonTest.php
+++ tests/JsonTest.php
@@ -55,44 +55,42 @@ class JsonTest extends ApiCase
         $this->seeJsonStructure($this->getKeys($this->list));
     }
 
-        public function testSort()
-        {
-            $this->json('GET', '/api/user?sort=-id');
-            $response = $this->response;
-            $this->assertEquals(200, $response->getStatusCode());
-            $this->assertEquals(config('jsonapi.content_type'), $response->headers->get('Content-type'));
-            $this->seeJsonStructure($this->getKeys($this->list));
-        }
+    public function testSort()
+    {
+        $this->json('GET', '/api/user?sort=-id');
+        $response = $this->response;
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(config('jsonapi.content_type'), $response->headers->get('Content-type'));
+        $this->seeJsonStructure($this->getKeys($this->list));
+    }
 
-        public function testBadSort()
-        {
-            $this->json('GET', '/api/user?sort=-test');
-            $response = $this->response;
-            $this->assertEquals(400, $response->getStatusCode());
-            $this->assertEquals(config('jsonapi.content_type'), $response->headers->get('Content-type'));
-        }
+    public function testBadSort()
+    {
+        $this->json('GET', '/api/user?sort=-test');
+        $response = $this->response;
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals(config('jsonapi.content_type'), $response->headers->get('Content-type'));
+    }
 
-        public function testListWithFields()
-        {
-            $this->createUser();
+    public function testListWithFields()
+    {
+        $this->createUser();
 
-            $this->json('GET', '/api/user?fields[user]=id,name');
-            $response = $this->response;
-            $this->assertEquals(200, $response->getStatusCode());
-            $this->assertEquals(config('jsonapi.content_type'), $response->headers->get('Content-type'));
+        $this->json('GET', '/api/user?fields[user]=id,name');
+        $response = $this->response;
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(config('jsonapi.content_type'), $response->headers->get('Content-type'));
+    }
 
-        }
+    public function testListWithBadFields()
+    {
+        $this->createUser();
 
-           public function testListWithBadFields()
-            {
-                $this->createUser();
-
-                $this->json('GET', '/api/user?fields[user]=id,name,bad');
-                $response = $this->response;
-                $this->assertEquals(400, $response->getStatusCode());
-                $this->assertEquals(config('jsonapi.content_type'), $response->headers->get('Content-type'));
-
-            }
+        $this->json('GET', '/api/user?fields[user]=id,name,bad');
+        $response = $this->response;
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals(config('jsonapi.content_type'), $response->headers->get('Content-type'));
+    }
 
     public function testError404()
     {
@@ -117,7 +115,6 @@ class JsonTest extends ApiCase
         $this->assertEquals(400, $response->getStatusCode());
         $this->assertEquals(config('jsonapi.content_type'), $response->headers->get('Content-type'));
     }
-
 
     public function testSearch()
     {
